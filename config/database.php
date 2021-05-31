05604337a21171d8308e6486a34b2539a744c783
Bachurahul33 <38493980+Bachurahul33@users.noreply.github.com>
2021-05-31 17:44:27 +0530

    Update database.php

--- config/database.php
+++ config/database.php
@@ -68,7 +68,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'test8'),
+            'database' => env('DB_DATABASE', 'test8db'),
             'username' => env('DB_USERNAME', 'postgres'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
