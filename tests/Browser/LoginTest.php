0dff66868b4711abb03fc9f3f1307cafb81e7ea5
Bachurahul33 <38493980+Bachurahul33@users.noreply.github.com>
2021-06-01 18:24:16 +0530

    Update LoginTest.php

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -13,30 +13,30 @@ class LoginTest extends DuskTestCase
      *@test
      * @return void
      */
-   public function test_login()
+   public function test_Register()
     {
         $this->browse(function (Browser $browser) {
-              $browser->visit('/login')
-                    ->assertSee('Login')
-                    ->type('email', 'bachurahul@gmail.com')
-                    ->type('password', '123456789')
-                    ->press('LOG IN')
-                    ->assertPathIs('/dashboard')
-                    ->assertSee('Dashboard')
-                    ->assertAuthenticated();
-        });
-    }
-
-    public function test_Register()
-    {
-        $this->browse(function (Browser $browser) {
-                    $browser->visit('/register')
+            $browser->visit('/register')
                     ->assertSee('Register')
                     ->type('name', 'Admin')
                     ->type('email', 'bachurahul@gmail.com')
                     ->type('password', '123456789')
                     ->type('password_confirmation', '123456789')
                     ->press('REGISTER')
+                    ->assertPathIs('/dashboard')
+                    ->assertSee('Dashboard')
+                    ->assertAuthenticated();
+        });
+    }
+
+    public function test_Login()
+    {
+        $this->browse(function (Browser $browser) {
+                    $browser->visit('/login')
+                    ->assertSee('Login')
+                    ->type('email', 'bachurahul@gmail.com')
+                    ->type('password', '123456789')
+                    ->press('LOG IN')
                     ->assertPathIs('/dashboard')
                     ->assertSee('Dashboard')
                     ->assertAuthenticated();
